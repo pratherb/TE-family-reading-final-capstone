22ca2eb0dcb089eb9d64d41d44d73ed82a1f63dd
Ava Lindstrom <ava.ela.lindstrom@gmail.com>
2023-05-31 15:23:36 -0500

    add delete user and view family members methods to back end

--- final-capstone/capstone/java/src/main/java/com/techelevator/controller/UserController.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/controller/UserController.java
@@ -3,8 +3,13 @@ package com.techelevator.controller;
 import com.techelevator.dao.FamilyDao;
 import com.techelevator.dao.UserDao;
 import com.techelevator.model.User;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -35,8 +40,18 @@ public class UserController {
     }
 
     @RequestMapping(value = ENDPOINT, method = RequestMethod.GET)
-    public List<User> getUsers(){
-        return userDao.findAll();
+    public List<User> getUsersByFamilyId(Principal principal){
+        int id = familyDao.getFamilyIdByUsername(principal.getName());
+        return userDao.getUsersByFamilyId(id);
+    }
+
+    @RequestMapping(value = ENDPOINT, method = RequestMethod.PUT)
+    public void deleteUserByUsername(String username, Principal principal){
+        if (principal.getName().equals(username)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cannot delete current user");
+        } else {
+            userDao.delete(username);
+        }
     }
 
 }
