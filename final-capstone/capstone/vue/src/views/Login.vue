91f2459c30f4e0ab53c6b54d218b1e0ffacaa055
Nova Widdifield <novawidd@gmail.com>
2023-06-01 14:25:17 -0600

    login in/register fix

--- final-capstone/capstone/vue/src/views/Login.vue
+++ final-capstone/capstone/vue/src/views/Login.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="login">
-      
+     
     <form @submit.prevent="login">
-      <h1 >Log In</h1>
+       <h1 class="image">
+        <img src="../images/sharpened-transparent-logo.png">
+      </h1>
+      <h1 class="welcome" > Welcome! Please Log In!</h1>
       <div role="alert" v-if="invalidCredentials">
         Invalid username and password!
       </div>
       <div role="alert" v-if="this.$route.query.registration">
         Thank you for registering, please sign in.
       </div>
-      <div class="form-input-group">
+<div class="all-input">
+      <div class="form-input-group" id="username">
         <label for="username">👤 Username</label>
         <input type="text" id="username" v-model="user.username" required autofocus />
       </div>
-      <div class="form-input-group">
+      <div class="form-input-group" id="password">
         <label for="password">🔒 Password</label>
         <input type="password" id="password" v-model="user.password" required />
       </div>
       <button type="submit">Sign in</button>
       <p>
       <router-link :to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
+      
+      </div>
       <div class="running-board">
         <p> 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 📓 📕 📙 📒 📗 📘 </p>
       </div>
     </form>
-    <div class="image">
-      <img src="../images/sharpened-transparent-logo.png">
-    </div>
     </div>
 </template>
 
@@ -79,11 +82,30 @@ form{
   color: rgb(88,85,99);
     font-family: 'Libre Baskerville', serif;
     font: rgb(88,85,99);
+    width: 100%;
+    height: 100%;
+}
+.all-input{
+  display: flex;
+  /* background-image: url('https://t4.ftcdn.net/jpg/00/22/56/07/360_F_22560741_69yMhsfAa5hndN8UxMTp2bhvkSqDPjCM.jpg'); */
+  background-image: url('file:///C:/Users/Student/Downloads/360_F_22560741_69yMhsfAa5hndN8UxMTp2bhvkSqDPjCM-removebg-preview.png');
+   background-repeat: no-repeat;
+  flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    align-content: center;
+    background-position: center;
+   height: 400px;
 }
 .form-input-group {
   font-family: 'Libre Baskerville', serif;
   margin-bottom: 1rem;
   color: rgb(88,85,99);
+      margin-top: 70px;
+    margin-right: 40px;
+}
+.running-board{
+  margin-top: 3%;
 }
 label {
    font-family: 'Libre Baskerville', serif;
@@ -91,11 +113,20 @@ label {
   font: rgb(88,85,99);
 }
 .image{
-   background-image:url('https://img.freepik.com/premium-photo/vintage-paper-texture-background-with-copy-space-space-text_213524-427.jpg?w=1380');
   display:flex;
 justify-content: center;
+height: 25%;
+margin-bottom: 5px;
+margin-top: 5px;
 }
 img{
   width:30%
+}
+.open-book{
+  display: flex;
+}
+.welcome {
+    display: flex;
+    justify-content: center;
 }
 </style>
