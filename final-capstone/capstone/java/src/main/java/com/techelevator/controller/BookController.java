efcbb4860d415643d5892c99f21589964e3f1e9f
Ava Lindstrom <ava.ela.lindstrom@gmail.com>
2023-06-01 15:27:44 -0500

    adding some book controller functionality

--- final-capstone/capstone/java/src/main/java/com/techelevator/controller/BookController.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/controller/BookController.java
@@ -1,23 +1,46 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.BookDao;
+import com.techelevator.dao.FamilyDao;
+import com.techelevator.model.Book;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 //@PreAuthorize("isAuthenticated()")
 public class BookController {
 
+    private FamilyDao familyDao;
     private BookDao bookDao;
     private final String ENDPOINT = "/book";
 
-    public BookController(BookDao bookDao) {
+    public BookController(BookDao bookDao, FamilyDao familyDao) {
         this.bookDao = bookDao;
+        this.familyDao = familyDao;
     }
 
     @RequestMapping(value = ENDPOINT + "/{title}", method = RequestMethod.GET)
     public String queryForBook(@PathVariable String title){
         return bookDao.searchBooksByTitle(title);
+    }
+
+    @RequestMapping(value = ENDPOINT, method = RequestMethod.POST)
+    public Book addBookToReadingList(Book book, Principal principal) {
+        return bookDao.addBookToReadingList(book, principal.getName());
+    }
+
+    @RequestMapping(value = ENDPOINT + "all", method = RequestMethod.GET)
+    public List<Book> getFamilyReadingList(Principal principal) {
+        int familyId = familyDao.getFamilyIdByUsername(principal.getName());
+        return bookDao.getFamilyReadingList(familyId);
+    }
+
+    @RequestMapping(value = ENDPOINT + "/{id}", method = RequestMethod.GET)
+    public List<Book> getUserReadingList(String username) {
+        return bookDao.getUserReadingList(username);
     }
 }
--- final-capstone/capstone/java/src/main/java/com/techelevator/dao/BookDao.java
+++ final-capstone/capstone/java/src/main/java/com/techelevator/dao/BookDao.java
@@ -8,4 +8,10 @@ public interface BookDao {
 
     String searchBooksByTitle(String title);
 
+    Book addBookToReadingList(Book book, String username);
+
+    List<Book> getFamilyReadingList(int familyId);
+
+    List<Book> getUserReadingList(String username);
+
 }
