10158dddd931f546ba12ef60b954791057de74d7
Vincent Patterson <vinceip532@gmail.com>
2023-06-04 11:14:40 -0400

    Add book detail

---
+++ final-capstone/capstone/vue/src/components/BookDetail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h4>{{book.title}}</h4>
+      <p>Author: {{book.author}}</p>
+      <p>ISBN: {{book.isbn}}</p>
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
