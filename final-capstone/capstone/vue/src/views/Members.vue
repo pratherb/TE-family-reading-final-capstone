e3e7761409264ab8b302dc286d123edc2c69263c
Nova Widdifield <novawidd@gmail.com>
2023-06-06 13:24:01 -0600

    fixed add classes

--- final-capstone/capstone/vue/src/views/Members.vue
+++ final-capstone/capstone/vue/src/views/Members.vue
@@ -2,29 +2,36 @@
   <div class="members">
     <div class="logo_header">
       <h3 id="member-heading">Your Family Members</h3>
-      <img src="../images/sharpened-transparent-logo.png">
-      </div>
+      <img src="../images/sharpened-transparent-logo.png" />
+    </div>
     <div class="loading" v-if="isLoading">
       <img src="../assets/book_pages_opening.gif" />
     </div>
     <h2>BookBound Image Here</h2>
     <div id="nav">
-      <button class="selections"><router-link v-bind:to="{ name: 'reading' }"> Reading</router-link></button>
-      <button class="selections"><router-link v-bind:to="{ name: 'prizes' }"> Prizes</router-link></button>
-      <button class="selections"><router-link v-bind:to="{ name: 'settings' }"> Settings</router-link></button>
+      <button class="selections">
+        <router-link v-bind:to="{ name: 'reading' }"> Reading</router-link>
+      </button>
+      <button class="selections">
+        <router-link v-bind:to="{ name: 'prizes' }"> Prizes</router-link>
+      </button>
+      <button class="selections">
+        <router-link v-bind:to="{ name: 'settings' }"> Settings</router-link>
+      </button>
     </div>
-     <table>
-    <tr v-for="member in memberResults" v-bind:key="member.id">
-      <td>
-        <router-link  class="username"
-          v-bind:to="{
-            name: 'user-profile',
-            params: { username: member.username },
-          }"
-          >{{ member.username }}</router-link
-        >
-      </td>
-    </tr>
+    <table>
+      <tr v-for="member in memberResults" v-bind:key="member.id">
+        <td>
+          <router-link
+            class="username"
+            v-bind:to="{
+              name: 'user-profile',
+              params: { username: member.username },
+            }"
+            >{{ member.username }}</router-link
+          >
+        </td>
+      </tr>
     </table>
     <div class="addButton">
       <router-link to="/addmember">
@@ -45,16 +52,16 @@ export default {
       isLoading: true,
       errorMsg: "",
       filterText: "",
-      memberResults: []
+      memberResults: [],
     };
   },
   created() {
     docsService.list().then((response) => {
       this.memberResults = response.data;
-      this.$store.commit('ADD_MEMBERS', this.memberResults);
+      this.$store.commit("ADD_MEMBERS", this.memberResults);
       this.isLoading = false;
     });
-  }
+  },
 };
 </script>
 
@@ -62,15 +69,15 @@ export default {
 @import url("https://fonts.cdnfonts.com/css/socake");
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap");
 
-.members{
- background: rgba(254, 209, 113, 0.5);
+.members {
+  background: rgba(254, 209, 113, 0.5);
 }
-#member-heading{
+#member-heading {
   font-family: "Socake", sans-serif;
   color: rgb(88, 85, 91);
 }
 
-.addButton{
+.addButton {
   margin-top: 12px;
 }
 .selections {
@@ -93,46 +100,45 @@ h2 {
   font-size: 48px;
 }
 
-#member-heading{
+#member-heading {
   color: rgb(88, 85, 91);
   font-size: 50px;
 }
 
-
-#nav a{
-   color: rgb(88, 85, 91);
+#nav a {
+  color: rgb(88, 85, 91);
   text-decoration: none;
 }
-table{
+table {
   border: 1px solid;
   margin: 20px auto 20px auto;
   /* top, right, bottom, left */
   background: white;
 }
-td{
-  border-bottom:solid;
-  width: 200PX;
+td {
+  border-bottom: solid;
+  width: 200px;
 }
-.username{
+.username {
   font-family: "Montserrat", sans-serif;
-   color: rgb(88, 85, 91);
+  color: rgb(88, 85, 91);
   font-size: 30px;
   text-decoration: none;
 }
-.logo_header{
+.logo_header {
   border: solid 5px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    height: 250px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
 }
-.logo_header img{
+.logo_header img {
   width: 250px;
   height: 350px;
 }
-.loading{
- flex-direction: column;
-    width: 200px;
+.loading {
+  flex-direction: column;
+  width: 200px;
 }
 </style>
